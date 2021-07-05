37a66f9500ae4b42c6f604df8dc1a9bfb9d3f9d5
venky <vbrawny@users.noreply.github.com>
2021-07-05 19:12:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import React, { useState } from "react";
+import { Select, MenuItem, FormControl, InputLabel } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import "./styles.css";
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    minWidth: 100
+  }
+}));
+
 export default function App() {
+  const [value, setValue] = useState("");
+  const classes = useStyles();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <h1>Hello from Victor</h1>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Numbers</InputLabel>
+        <Select onChange={handleChange}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+          <MenuItem value={40}>Fourty</MenuItem>
+        </Select>
+      </FormControl>
+      <p>You Selected: {value}</p>
     </div>
   );
 }
